5f90fd285f0511a09b75c213d7baf35fbc3fdfe2
Hristian Carabulea <hristian.carabulea@outlook.com>
2021-12-08 19:49:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NumOfDaysYouStillHaveToLive
+
+## This repository is for calculating in days, in weeks, and in months how long you still have to live based on your current age and the age at which you think you will die.
+
+### I am doing my part in bringing joy to your life. 🍷🍾🍺🍻🥂🍸🍹
---
+++ app.js
@@ -0,0 +1,20 @@
+function lifeInWeeks(deathAge, age) {
+
+  //var deathAge = 70;
+  var yearsLeftToLive;
+  var monthsLeftToLive;
+  var weeksLeftToLive;
+  var daysLeftToLive;
+
+  yearsLeftToLive = parseInt(deathAge) - parseInt(age) ;
+  monthsLeftToLive = yearsLeftToLive * 12;
+  weeksLeftToLive = yearsLeftToLive * 52;
+  daysLeftToLive = yearsLeftToLive * 365;
+  alert("You have " + daysLeftToLive + " days, " + weeksLeftToLive + " weeks, or " + monthsLeftToLive + " months left to live until you will die at the age of " + deathAge + ".");
+  //console.log("You have " + daysLeftToLive + " days, " + weeksLeftToLive + " weeks, and " + monthsLeftToLive + " months left.");
+}
+
+var deathAge = prompt("At what age you think you will die? ");
+var yourAge = prompt("Enter your age: ");
+
+lifeInWeeks(deathAge, yourAge);
